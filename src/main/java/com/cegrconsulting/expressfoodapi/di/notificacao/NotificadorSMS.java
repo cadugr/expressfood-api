e36387206833412c1fc3aa6ca9c186e85ee418a4
Cadu Guerra <cadu.guerra@gmail.com>
2023-03-21 18:22:40 -0300

    Finalizando a aula com resolução de ambiguidade de beans com anotação @Primary

--- src/main/java/com/cegrconsulting/expressfoodapi/di/notificacao/NotificadorSMS.java
+++ src/main/java/com/cegrconsulting/expressfoodapi/di/notificacao/NotificadorSMS.java
@@ -1,9 +1,11 @@
 package com.cegrconsulting.expressfoodapi.di.notificacao;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import com.cegrconsulting.expressfoodapi.di.modelo.Cliente;
 
+@Primary
 @Component
 public class NotificadorSMS implements Notificador {
 
--- src/main/java/com/cegrconsulting/expressfoodapi/di/service/AtivacaoClienteService.java
+++ src/main/java/com/cegrconsulting/expressfoodapi/di/service/AtivacaoClienteService.java
@@ -1,7 +1,5 @@
 package com.cegrconsulting.expressfoodapi.di.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,25 +10,12 @@ import com.cegrconsulting.expressfoodapi.di.notificacao.Notificador;
 public class AtivacaoClienteService {
 
 	@Autowired
-	private List<Notificador> notificadores;
-
-	// @Autowired
-	// public AtivacaoClienteService(Notificador notificador) {
-	// 	this.notificador = notificador;
-	// }
+	private Notificador notificador;
 	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		
-		for(Notificador notificador : notificadores) {
-			notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-		}
-		
+		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
-
-	// @Autowired
-	// public void setNotificador(Notificador notificador) {
-	// 	this.notificador = notificador;
-	// }
 
 }
