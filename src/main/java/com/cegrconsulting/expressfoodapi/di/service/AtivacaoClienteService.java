08f5ddc9e85978bf3e6e3cf495b5c43ecec766a7
Cadu Guerra <cadu.guerra@gmail.com>
2023-03-21 17:39:11 -0300

    Finalizando a aula.

--- src/main/java/com/cegrconsulting/expressfoodapi/di/service/AtivacaoClienteService.java
+++ src/main/java/com/cegrconsulting/expressfoodapi/di/service/AtivacaoClienteService.java
@@ -1,24 +1,31 @@
 package com.cegrconsulting.expressfoodapi.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.cegrconsulting.expressfoodapi.di.modelo.Cliente;
 import com.cegrconsulting.expressfoodapi.di.notificacao.Notificador;
 
-
+@Component
 public class AtivacaoClienteService {
 
+	@Autowired
 	private Notificador notificador;
 
-
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-	  System.out.println("AtivacaoClienteService: " + notificador); 
-	}
-
+	// @Autowired
+	// public AtivacaoClienteService(Notificador notificador) {
+	// 	this.notificador = notificador;
+	// }
 	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
-	
+
+	// @Autowired
+	// public void setNotificador(Notificador notificador) {
+	// 	this.notificador = notificador;
+	// }
+
 }
