9319f716121009a0989373650d2c122eb3bbb536
Cadu Guerra <cadu.guerra@gmail.com>
2023-03-22 10:53:50 -0300

    Finalizando a aula.

--- src/main/java/com/cegrconsulting/expressfoodapi/di/notificacao/NotificadorEmailMock.java
+++ src/main/java/com/cegrconsulting/expressfoodapi/di/notificacao/NotificadorEmailMock.java
@@ -5,19 +5,18 @@ import org.springframework.stereotype.Component;
 
 import com.cegrconsulting.expressfoodapi.di.modelo.Cliente;
 
-@Profile("prod")
-@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
+@Profile("dev")
+@TipoDoNotificador(NivelUrgencia.URGENTE)
 @Component
-public class NotificadorEmail implements Notificador {
+public class NotificadorEmailMock implements Notificador {
 
-
-	public NotificadorEmail () {
-		System.out.println("NotificadorEmail REAL");
+	public NotificadorEmailMock () {
+		System.out.println("NotificadorEmail MOCK");
 	}
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
+		System.out.printf("MOCK: Notificação seria enviada para %s através do e-mail %s: %s\n", 
 				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
