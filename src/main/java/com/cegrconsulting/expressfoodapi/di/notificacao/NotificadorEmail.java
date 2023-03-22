01c5444b0b177561646f26c9e49794721188b357
Cadu Guerra <cadu.guerra@gmail.com>
2023-03-22 13:16:01 -0300

    Finalizando a aula.

--- src/main/java/com/cegrconsulting/expressfoodapi/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/cegrconsulting/expressfoodapi/di/notificacao/NotificadorEmail.java
@@ -1,5 +1,6 @@
 package com.cegrconsulting.expressfoodapi.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.cegrconsulting.expressfoodapi.di.modelo.Cliente;
@@ -8,8 +9,18 @@ import com.cegrconsulting.expressfoodapi.di.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 
+	@Value("${notificador.email.host-servidor}")
+	private String host;
+
+	@Value("${notificador.email.porta-servidor}")
+	private Integer porta;
+
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+
+		System.out.println("host: " + host);
+		System.out.println("porta: " + porta);
+
 		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
 				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
