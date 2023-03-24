fc342f1c59d8025b3d057a36c701241d7f6f0b28
Cadu Guerra <cadu.guerra@gmail.com>
2023-03-24 11:10:10 -0300

    Excluindo um objeto do banco de dados

---
+++ src/main/java/com/cegrconsulting/expressfood/jpa/ExclusaoCozinhaMain.java
@@ -0,0 +1,26 @@
+package com.cegrconsulting.expressfood.jpa;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.cegrconsulting.expressfood.ExpressfoodApiApplication;
+import com.cegrconsulting.expressfood.domain.model.Cozinha;
+
+public class ExclusaoCozinhaMain {
+
+  public static void main(String[] args) {
+    ApplicationContext applicationContext = new SpringApplicationBuilder(ExpressfoodApiApplication.class)
+            .web(WebApplicationType.NONE)
+            .run(args);
+
+    CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+    
+    Cozinha cozinha = new Cozinha();
+    cozinha.setId(1L);
+    
+    cadastroCozinha.remover(cozinha);
+    
+  }
+  
+}
