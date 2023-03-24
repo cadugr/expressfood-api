f05e10ee88e3fe3dc1cb5fbb4466f67e72125a58
Cadu Guerra <cadu.guerra@gmail.com>
2023-03-24 14:54:03 -0300

    A anotação @JoinColumn

--- src/main/java/com/cegrconsulting/expressfood/domain/model/Restaurante.java
+++ src/main/java/com/cegrconsulting/expressfood/domain/model/Restaurante.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import lombok.Data;
@@ -28,6 +29,7 @@ public class Restaurante {
 	private BigDecimal taxaFrete;
 
 	@ManyToOne
+	@JoinColumn(name = "cozinha_id") //neste caso nem precisava, pois esse é o nome de coluna criado por padrão pelo Hibernate
 	private Cozinha cozinha;
 
 }
