78162089de2b6b7993c11a1437ebe2e8709891a8
Cadu Guerra <cadu.guerra@gmail.com>
2023-03-26 15:02:09 -0300

    Customizando a representação em XML com Wrapper e anotações do Jackson

--- src/main/java/com/cegrconsulting/expressfood/api/controller/CozinhaController.java
+++ src/main/java/com/cegrconsulting/expressfood/api/controller/CozinhaController.java
@@ -3,11 +3,13 @@ package com.cegrconsulting.expressfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cegrconsulting.expressfood.api.model.CozinhasXmlWrapper;
 import com.cegrconsulting.expressfood.domain.model.Cozinha;
 import com.cegrconsulting.expressfood.domain.repository.CozinhaRepository;
 
@@ -21,6 +23,11 @@ public class CozinhaController {
   @GetMapping
   public List<Cozinha> listar() {
     return cozinhaRepository.listar();
+  }
+
+  @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+  public CozinhasXmlWrapper listarXml() {
+    return new CozinhasXmlWrapper(cozinhaRepository.listar());
   }
 
   @GetMapping("/{cozinhaId}")
---
+++ src/main/java/com/cegrconsulting/expressfood/api/model/CozinhasXmlWrapper.java
@@ -0,0 +1,22 @@
+package com.cegrconsulting.expressfood.api.model;
+
+import java.util.List;
+
+import com.cegrconsulting.expressfood.domain.model.Cozinha;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+import lombok.Data;
+import lombok.NonNull;
+
+@JacksonXmlRootElement(localName = "cozinhas") //poderíamos utilizar a anotação JsonRootElement, daria na mesma
+@Data
+public class CozinhasXmlWrapper {
+
+  @JacksonXmlProperty(localName = "cozinha")
+  @JacksonXmlElementWrapper(useWrapping = false)
+  @NonNull //Os construtores gerados pela anotação @Data só são feitos para propriedades com esta anotação. 
+  private List<Cozinha> cozinhas;
+  
+}
