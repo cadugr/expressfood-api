84c969f445ac6fdaf49482b0b49a2f5b3d9720f9
Cadu Guerra <cadu.guerra@gmail.com>
2023-03-26 21:22:02 -0300

    Corrigindo o Status HTTP para resource inexistente

--- src/main/java/com/cegrconsulting/expressfood/api/controller/CozinhaController.java
+++ src/main/java/com/cegrconsulting/expressfood/api/controller/CozinhaController.java
@@ -3,8 +3,6 @@ package com.cegrconsulting.expressfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,13 +33,13 @@ public class CozinhaController {
 
   @GetMapping("/{cozinhaId}")
   public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
-    Cozinha cozinha =  cozinhaRepository.buscar(cozinhaId);
-   // return ResponseEntity.status(HttpStatus.OK).body(cozinha);
-   //return ResponseEntity.ok(cozinha);
-   HttpHeaders headers = new HttpHeaders();
-   headers.add(HttpHeaders.LOCATION, "http://api.expressfood.local:8080/cozinhas");
-   return ResponseEntity.status(HttpStatus.FOUND)
-    .headers(headers).build();
+    Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+    if(cozinha != null) {
+      return ResponseEntity.ok(cozinha);
+    }
+
+    return ResponseEntity.notFound().build();
   }
   
 }
