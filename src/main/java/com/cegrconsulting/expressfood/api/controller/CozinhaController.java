126a98f25a34957d080b61fb667b3aad4bd851cc
Cadu Guerra <cadu.guerra@gmail.com>
2023-03-26 21:12:49 -0300

    Manipulando a resposta HTTP com ResponseEntity

--- src/main/java/com/cegrconsulting/expressfood/api/controller/CozinhaController.java
+++ src/main/java/com/cegrconsulting/expressfood/api/controller/CozinhaController.java
@@ -3,12 +3,13 @@ package com.cegrconsulting.expressfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cegrconsulting.expressfood.api.model.CozinhasXmlWrapper;
@@ -32,10 +33,15 @@ public class CozinhaController {
     return new CozinhasXmlWrapper(cozinhaRepository.listar());
   }
 
-  @ResponseStatus(HttpStatus.CREATED)
   @GetMapping("/{cozinhaId}")
-  public Cozinha buscar(@PathVariable Long cozinhaId) {
-    return cozinhaRepository.buscar(cozinhaId);
+  public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+    Cozinha cozinha =  cozinhaRepository.buscar(cozinhaId);
+   // return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+   //return ResponseEntity.ok(cozinha);
+   HttpHeaders headers = new HttpHeaders();
+   headers.add(HttpHeaders.LOCATION, "http://api.expressfood.local:8080/cozinhas");
+   return ResponseEntity.status(HttpStatus.FOUND)
+    .headers(headers).build();
   }
   
 }
