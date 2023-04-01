7a5230e8078e83761379c4087a8c052907b16fab
Cadu Guerra <cadu.guerra@gmail.com>
2023-04-01 16:13:05 -0300

    Desafio: Modelando e implementando a atualização de recursos de restaurantes

--- src/main/java/com/cegrconsulting/expressfood/api/controller/RestauranteController.java
+++ src/main/java/com/cegrconsulting/expressfood/api/controller/RestauranteController.java
@@ -2,12 +2,14 @@ package com.cegrconsulting.expressfood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,6 +55,23 @@ public class RestauranteController {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
     
+  }
+
+  @PutMapping("/{restauranteId}")
+  public ResponseEntity<?> atualizar(@PathVariable Long restauranteId, 
+        @RequestBody Restaurante restaurante) {
+    Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+    if(restauranteAtual == null) {
+      return ResponseEntity.notFound().build();
+    }        
+
+    BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+    try {
+      restauranteAtual = cadastroRestaurante.salvar(restauranteAtual);
+      return ResponseEntity.ok().body(restauranteAtual);
+    } catch (EntidadeNaoEncontradaException e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
   }
   
 }
