ac27a2bf99cf03a0e5eacf5b23ad675ebe13b0de
Cadu Guerra <cadu.guerra@gmail.com>
2023-04-04 15:15:08 -0300

    Refatorando o código do projeto para usar o repositório do SDJ

--- src/main/java/com/cegrconsulting/expressfood/api/controller/TesteController.java
+++ src/main/java/com/cegrconsulting/expressfood/api/controller/TesteController.java
@@ -1,25 +1,18 @@
 package com.cegrconsulting.expressfood.api.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.cegrconsulting.expressfood.domain.model.Cozinha;
-import com.cegrconsulting.expressfood.domain.repository.CozinhaRepository;
 
 @RestController
 @RequestMapping("/teste")
 public class TesteController {
 
-  @Autowired
-  private CozinhaRepository cozinhaRepository;
+  // @Autowired
+  // private CozinhaRepository cozinhaRepository;
 
-  @GetMapping("/cozinhas/por-nome")
-  public List<Cozinha> cozinhasPorNome(String nome) {
-    return cozinhaRepository.consultarPorNome(nome);
-  }
+  // @GetMapping("/cozinhas/por-nome")
+  // public List<Cozinha> cozinhasPorNome(String nome) {
+  //   return cozinhaRepository.consultarPorNome(nome);
+  // }
   
 }
--- src/main/java/com/cegrconsulting/expressfood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/cegrconsulting/expressfood/domain/repository/CozinhaRepository.java
@@ -1,15 +1,13 @@
 package com.cegrconsulting.expressfood.domain.repository;
 
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cegrconsulting.expressfood.domain.model.Cozinha;
 
-public interface CozinhaRepository {
+@Repository
+public interface CozinhaRepository extends JpaRepository<Cozinha, Long>{
 
-  List<Cozinha> listar();
-  List<Cozinha> consultarPorNome(String nome);
-  Cozinha buscar(Long id);
-  Cozinha salvar(Cozinha cozinha);
-  void remover(Long id);
+  //List<Cozinha> consultarPorNome(String nome);
   
 }
