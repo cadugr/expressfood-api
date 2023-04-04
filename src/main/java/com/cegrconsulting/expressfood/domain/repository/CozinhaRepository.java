e3785644999df0ecb7c53d52fd120001e3468d3f
Cadu Guerra <cadu.guerra@gmail.com>
2023-04-04 13:23:14 -0300

    Implementando consultas JPQL em repositórios

--- src/main/java/com/cegrconsulting/expressfood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/cegrconsulting/expressfood/domain/repository/CozinhaRepository.java
@@ -7,6 +7,7 @@ import com.cegrconsulting.expressfood.domain.model.Cozinha;
 public interface CozinhaRepository {
 
   List<Cozinha> listar();
+  List<Cozinha> consultarPorNome(String nome);
   Cozinha buscar(Long id);
   Cozinha salvar(Cozinha cozinha);
   void remover(Long id);
--- src/main/java/com/cegrconsulting/expressfood/infrastructure/repository/FormaPagamentoRepositoryImpl.java
+++ src/main/java/com/cegrconsulting/expressfood/infrastructure/repository/FormaPagamentoRepositoryImpl.java
@@ -5,13 +5,13 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cegrconsulting.expressfood.domain.model.FormaPagamento;
 import com.cegrconsulting.expressfood.domain.repository.FormaPagamentoRepository;
 
-@Component
+@Repository
 public class FormaPagamentoRepositoryImpl implements FormaPagamentoRepository {
 
   @PersistenceContext
