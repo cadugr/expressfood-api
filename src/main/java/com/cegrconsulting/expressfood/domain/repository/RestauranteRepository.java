fc17c1b5ade55d3bf54c8a7b08f176f31ef29298
Cadu Guerra <cadu.guerra@gmail.com>
2023-04-08 23:07:20 -0300

    Usando queries JPQL customizadas com @Query

--- src/main/java/com/cegrconsulting/expressfood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/cegrconsulting/expressfood/domain/repository/RestauranteRepository.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.cegrconsulting.expressfood.domain.model.Restaurante;
@@ -13,7 +15,9 @@ import com.cegrconsulting.expressfood.domain.model.Restaurante;
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
   List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
-  List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
+  @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+  List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
+  //List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
   Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome); //vai buscar o primeiro registro, mesmo que hajam outros.
   List<Restaurante> findTop2ByNomeContaining(String nome); //Usando o Top seguido de um número, estamos buscando os primeiros registros.  Neste caso, os dois primeiros.
   int countByCozinhaId(Long cozinhaId); //Utilizamos o count para contar a quantidade de registros.  Neste caso, desejamos saber a quantidade de restaurantes, dada uma cozinha
