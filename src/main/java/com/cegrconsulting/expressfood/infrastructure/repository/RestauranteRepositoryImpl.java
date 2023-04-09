09a1ff45ef43504a24636bf0ec1aa9a872eedf68
Cadu Guerra <cadu.guerra@gmail.com>
2023-04-09 16:10:23 -0300

    Implementando uma consulta simples com Criteria API

--- src/main/java/com/cegrconsulting/expressfood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/cegrconsulting/expressfood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,15 +1,15 @@
 package com.cegrconsulting.expressfood.infrastructure.repository;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import com.cegrconsulting.expressfood.domain.model.Restaurante;
 import com.cegrconsulting.expressfood.domain.repository.RestauranteRepositoryQueries;
@@ -24,31 +24,13 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
   @Override
   public List<Restaurante> find(String nome, 
             BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
-              
-    var jpql = new StringBuilder(); 
-    jpql.append("from Restaurante where 0=0 ");
 
-    var parametros = new HashMap<String, Object>();
-
-    if(StringUtils.hasLength(nome)) {
-      jpql.append("and nome like :nome ");
-      parametros.put("nome", "%" + nome + "%");
-    }
+    CriteriaBuilder builder = manager.getCriteriaBuilder();          
+    CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class); //O CriteriaQuery é uma interface responsável por compor as cláusulas (where, from, group by, etc) das consultas.
     
-    if(taxaFreteInicial != null) {
-      jpql.append("and taxaFrete >= :taxaInicial ");
-      parametros.put("taxaInicial", taxaFreteInicial);
-    }
+    criteria.from(Restaurante.class);
 
-    if(taxaFreteFinal != null) {
-      jpql.append("and taxaFrete <= :taxaFinal");
-      parametros.put("taxaFinal", taxaFreteFinal);
-    }
-
-    TypedQuery<Restaurante> query =  manager
-              .createQuery(jpql.toString(), Restaurante.class);
-
-    parametros.forEach((chave, valor) -> query.setParameter(chave, valor));          
+    TypedQuery<Restaurante> query =  manager.createQuery(criteria);
     return query.getResultList();
         
   }
