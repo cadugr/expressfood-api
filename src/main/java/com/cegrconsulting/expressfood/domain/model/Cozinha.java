e92ab0bee7b4a21091e202a22c373213dc78f35f
Cadu Guerra <cadu.guerra@gmail.com>
2023-04-15 14:32:58 -0300

    Mapeando relacionamento bidirecional com @OneToMany

--- src/main/java/com/cegrconsulting/expressfood/domain/model/Cozinha.java
+++ src/main/java/com/cegrconsulting/expressfood/domain/model/Cozinha.java
@@ -1,11 +1,16 @@
 package com.cegrconsulting.expressfood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.Data;
@@ -24,5 +29,10 @@ public class Cozinha {
 
   @Column(nullable = false)
   private String nome;
+
+  @JsonIgnore
+  @OneToMany(mappedBy = "cozinha") //A entidade restaurante é a dona do relacionamento, pois a tabela restaurante possui o id da cozinha
+  //Relacionamentos bidirecionais sem necessidade, são considerados uma má prática
+  private List<Restaurante> restaurantes = new ArrayList<>();
 
 }
