5164fe1a806953a35c22e7e396633bc71bc36c8c
Cadu Guerra <cadu.guerra@gmail.com>
2023-04-15 11:37:14 -0300

    Injetando o próprio repositório na implementação customizada e a anotação @Lazy

--- src/main/java/com/cegrconsulting/expressfood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/cegrconsulting/expressfood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,5 +1,8 @@
 package com.cegrconsulting.expressfood.infrastructure.repository;
 
+import static com.cegrconsulting.expressfood.infrastructure.repository.spec.RestauranteSpecs.comFreteGratis;
+import static com.cegrconsulting.expressfood.infrastructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,10 +15,13 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import com.cegrconsulting.expressfood.domain.model.Restaurante;
+import com.cegrconsulting.expressfood.domain.repository.RestauranteRepository;
 import com.cegrconsulting.expressfood.domain.repository.RestauranteRepositoryQueries;
 
 //É preciso que a classe tenha este sufixo, caso contrário não irá funcionar nosso repositório customizado
@@ -24,6 +30,10 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
   @PersistenceContext
   private EntityManager manager;
+
+  @Autowired
+  @Lazy //Com essa anotação evitamos o problema de referência circular, pois estamos informando ao Spring para instanciar este tipo, somente quando for preciso, ou seja, quando formos utilizar o mesmo.
+  private RestauranteRepository restauranteRepository;
 
   @Override
   public List<Restaurante> find(String nome, 
@@ -54,6 +64,12 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
     TypedQuery<Restaurante> query =  manager.createQuery(criteria);
     return query.getResultList();
         
+  }
+
+  @Override
+  public List<Restaurante> findComFreteGratis(String nome) {
+    return restauranteRepository.findAll(comFreteGratis()
+                  .and(comNomeSemelhante(nome)));
   }
   
 }
