43538fef89e697c75d930b4b035a0e115d1114cf
Cadu Guerra <cadu.guerra@gmail.com>
2023-04-29 21:02:59 -0300

    Desafio: usando a exception NegocioException

--- src/main/java/com/cegrconsulting/expressfood/api/controller/RestauranteController.java
+++ src/main/java/com/cegrconsulting/expressfood/api/controller/RestauranteController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cegrconsulting.expressfood.domain.exception.EntidadeNaoEncontradaException;
+import com.cegrconsulting.expressfood.domain.exception.NegocioException;
 import com.cegrconsulting.expressfood.domain.model.Restaurante;
 import com.cegrconsulting.expressfood.domain.repository.RestauranteRepository;
 import com.cegrconsulting.expressfood.domain.service.CadastroRestauranteService;
@@ -46,7 +48,11 @@ public class RestauranteController {
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public Restaurante adicionar(@RequestBody Restaurante restaurante) {
+    try {
       return cadastroRestaurante.salvar(restaurante);
+    } catch (EntidadeNaoEncontradaException e) {
+      throw new NegocioException(e.getMessage());
+    }  
   }
 
   @PutMapping("/{restauranteId}")
@@ -55,7 +61,11 @@ public class RestauranteController {
     Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
     
     BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento", "endereco", "dataCadastro");
-    return cadastroRestaurante.salvar(restauranteAtual);
+    try {
+      return cadastroRestaurante.salvar(restauranteAtual);
+    } catch (EntidadeNaoEncontradaException e) {
+      throw new NegocioException(e.getMessage());
+    }
     
   }
 
