b80897e07234f81a3d61450e844226c4fc8a70e4
Cadu Guerra <cadu.guerra@gmail.com>
2023-05-01 21:04:08 -0300

    Lançando exception de desserialização na atualização parcial (PATCH)

--- src/main/java/com/cegrconsulting/expressfood/api/controller/RestauranteController.java
+++ src/main/java/com/cegrconsulting/expressfood/api/controller/RestauranteController.java
@@ -4,9 +4,14 @@ import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,6 +28,7 @@ import com.cegrconsulting.expressfood.domain.exception.NegocioException;
 import com.cegrconsulting.expressfood.domain.model.Restaurante;
 import com.cegrconsulting.expressfood.domain.repository.RestauranteRepository;
 import com.cegrconsulting.expressfood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
@@ -69,32 +75,44 @@ public class RestauranteController {
   }
 
   @PatchMapping("/{restauranteId}")
-  public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos) {
+  public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos,
+                        HttpServletRequest request) {
     Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
 
-    merge(campos, restauranteAtual);
+    merge(campos, restauranteAtual, request);
 
     return atualizar(restauranteId, restauranteAtual);
   }
 
-  private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+  private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino, HttpServletRequest request) {
 
-    ObjectMapper objectMapper = new ObjectMapper();
-    Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class); // necessário para já
-                                                                                               // converter todos os
-                                                                                               // valores. Se não
-                                                                                               // fizermos, teremos
-                                                                                               // erros de conversão
+    ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
 
-    dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-      Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-      field.setAccessible(true); // permite que possamos acessar o atributo privado fora da classe
-      Object novoValor = ReflectionUtils.getField(field, restauranteOrigem); // Desta forma estamos buscando o campo que
-                                                                             // desejamos dentro do objeto
-                                                                             // restauranteOrigem
-
-      ReflectionUtils.setField(field, restauranteDestino, novoValor);
-    });
+    try {
+      ObjectMapper objectMapper = new ObjectMapper();
+      objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+      objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+  
+      Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class); // necessário para já
+                                                                                                 // converter todos os
+                                                                                                 // valores. Se não
+                                                                                                 // fizermos, teremos
+                                                                                                 // erros de conversão
+  
+      dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+        Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+        field.setAccessible(true); // permite que possamos acessar o atributo privado fora da classe
+        Object novoValor = ReflectionUtils.getField(field, restauranteOrigem); // Desta forma estamos buscando o campo que
+                                                                               // desejamos dentro do objeto
+                                                                               // restauranteOrigem
+  
+        ReflectionUtils.setField(field, restauranteDestino, novoValor);
+      });
+      
+    } catch (IllegalArgumentException e) {
+      Throwable rootCause = ExceptionUtils.getRootCause(e);
+      throw new HttpMessageNotReadableException(e.getMessage(), rootCause, serverHttpRequest);
+    }
   }
 
 }
