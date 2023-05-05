6c26abe0ef1354c7c6c33f13e70f81ed8cb97f19
Cadu Guerra <cadu.guerra@gmail.com>
2023-05-04 21:32:39 -0300

    Conhecendo e adicionando mais constraints de validação no modelo

--- src/main/java/com/cegrconsulting/expressfood/domain/model/Restaurante.java
+++ src/main/java/com/cegrconsulting/expressfood/domain/model/Restaurante.java
@@ -16,7 +16,8 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -36,10 +37,15 @@ public class Restaurante {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotNull
+	//@NotNull
+	//@NotEmpty
+	@NotBlank
 	@Column(nullable = false)
 	private String nome;
 	
+	//@DecimalMin("1")
+	//Uma outra anotação que faz o mesmo é a a seguir:
+	@PositiveOrZero
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 
